e04b6a71141ca665853046fc33351149f92e82e4
Jazgulya <peri_04.03.02@mail.ru>
2022-06-13 19:16:10 +0600

    added header navbar create read

--- src/Routing.js
+++ src/Routing.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddEvent from "./components/AddEvent/AddEvent";
+import EventsList from "./components/EventsList/EventsList";
+import EventsContextProvider from "./contexts/eventContext";
 
 const Routing = () => {
-    return (
-       
-        );
+  return (
+    <EventsContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/add-event" element={<AddEvent />} />
+          <Route path="/events" element={<EventsList />} />;
+        </Routes>
+      </BrowserRouter>
+    </EventsContextProvider>
+  );
 };
 
 export default Routing;
